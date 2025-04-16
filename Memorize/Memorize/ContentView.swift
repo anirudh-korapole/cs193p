963b6135b14397715e6726ecc4e644036d02fcb1
anirudh-korapole <anirudh.korapole@gmail.com>
2025-04-16 20:07:59 +0530

    Lecture 1 revisited and done. Learnt how Stacks work. TupleViews, Views etc. HStack, VStack and ZStack working learnt.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,34 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        
         HStack {
+
             CardView(isFaceUp: true)
             CardView()
             CardView()
             CardView()
         }
         .padding()
-        .foregroundColor(.orange)
+        .foregroundStyle(.orange)
+        
+        
     }
 }
-                    
-struct CardView: View {
+
+struct CardView : View {
     var isFaceUp: Bool = false
-    
     var body: some View {
         ZStack(content: {
             if isFaceUp {
                 RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
+                    .foregroundStyle(.white)
                 RoundedRectangle(cornerRadius: 12)
                     .strokeBorder(lineWidth: 2)
-                Text("👻").font(.title)
+
+                Text("👻").font(.largeTitle)
             } else {
                 RoundedRectangle(cornerRadius: 12)
             }
+            
         })
-        
-       
     }
 }
 
