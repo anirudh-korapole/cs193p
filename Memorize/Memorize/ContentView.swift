21d83a294f975b36f57a156b6a2cbe0a8992b1a5
anirudh-korapole <anirudh.korapole@gmail.com>
2025-04-18 17:26:38 +0530

    Lecture 2
    Added few changes, especially cleaned up some code. Added a LazyGridView. ScrollView.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,19 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: Array<String> = ["👻", "🎃", "🕷️", "😈"]
+    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹","😱","☠️","🍭"]
+    
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        
-       
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            ScrollView {
+                cards
+                
             }
+            Spacer()
+            cardCountAdjusters
+            
         }
         .padding()
-        .foregroundStyle(.orange)
         
-        
+    }
+    
+    
+    
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            HStack {
+                cardRemover
+                Spacer()
+                cardAdder
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+        }
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button (action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+            .foregroundStyle(.orange)
+            
+        }
     }
 }
 
@@ -34,14 +80,14 @@ struct CardView : View {
             let base = RoundedRectangle(cornerRadius: 12)
     
             	
-            if isFaceUp {
+            Group {
                     base.foregroundStyle(.white)
                     base.strokeBorder(lineWidth: 2)
 
                 Text(content).font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
             
         }
         .onTapGesture {
