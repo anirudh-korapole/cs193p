cca2e214df8894c7ddb00e6866381391a0adcc5f
anirudh-korapole <anirudh.korapole@gmail.com>
2025-07-23 14:10:28 +0530

    Lecture 4 developed the MVVM model

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -3,22 +3,37 @@
 //  Memorize
 //
 //  Created by Anirudh on 08/07/25.
-//
+// model
 
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
-    func choose(card: Card) {
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent ) {
+        cards = []
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content: CardContent = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    
+    func choose(_ card: Card) {
         
     }
     
     
     struct Card {
-        var isFaceUp : Bool
-        var isMatched: Bool
-        var content: CardContent
+        var isFaceUp = true
+        var isMatched = false
+        let content: CardContent
         
     }
 }
