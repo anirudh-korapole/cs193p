5c5548c277cb07c922426e9c4319d855f4fdc35a
anirudh-korapole <anirudh.korapole@gmail.com>
2025-07-24 21:24:04 +0530

    added few animations and added card flipping on touch gesture

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
-   
+    
     
     
     
@@ -26,52 +26,51 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         
-   
+        
     }
     
-
+    
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(card: viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                CardView(card: card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
-            .foregroundStyle(.orange)
             
         }
+        .foregroundStyle(.orange)
     }
+}
     
-    struct CardView : View {
+struct CardView : View {
         let card: MemoryGame<String>.Card
         
         
-        var body: some View {
-            ZStack {
-                let base = RoundedRectangle(cornerRadius: 12)
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            
+            Group {
+                base.foregroundStyle(.white)
+                base.strokeBorder(lineWidth: 2)
                 
-                
-                Group {
-                    base.foregroundStyle(.white)
-                    base.strokeBorder(lineWidth: 2)
-                    
-                    Text(card.content)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.01)
-                        .aspectRatio(1, contentMode: .fit)
-                }
-                .opacity(card.isFaceUp ? 1 : 0)
-                base.fill().opacity(card.isFaceUp ? 0 : 1)
-                
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
             }
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
             
         }
+        
     }
-    
-    
-    
-    
     
     
 }
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -15,22 +15,39 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         // add numberOfPairsOfCards x 2 cards
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let content: CardContent = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
         }
     }
     
     mutating func shuffle() {
         cards.shuffle()
+        print(cards)
     }
     
     
-    func choose(_ card: Card) {
+    mutating func choose(_ card: Card) {
+        let choosenIndex = index(of: card)
+        cards[choosenIndex].isFaceUp.toggle()
+        print("chose \(card)")
+    }
+    
+    func index(of card: Card) -> Int {
+        for index in cards.indices {
+            if cards[index].id == card.id {
+                return index
+            }
+        }
+        return 0 // FIXME: Bogus!
+    }
+    
+    
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var debugDescription: String {
+            return "\(id): \(content) \(isFaceUp ? "up" : "down")\(isMatched ? "matched" : "")"
+        }
         
-    }
-    
-    
-    struct Card: Equatable {
+        
         static func == (lhs: MemoryGame<CardContent>.Card, rhs: MemoryGame<CardContent>.Card) -> Bool {
             return lhs.isFaceUp == rhs.isFaceUp &&
             lhs.isMatched == rhs.isMatched &&
@@ -41,5 +58,6 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         var isMatched = false
         let content: CardContent
         
+        var id: String
     }
 }
