0323ffdb5bde9e0c98b326f4211d197ed2f3efd3
anirudh-korapole <anirudh.korapole@gmail.com>
2025-07-25 20:33:52 +0530

    Assignment 2 of the class, Building Memorize game from scratch with scores and such features added

---
+++ More Memorise A2/More Memorise A2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  More Memorise A2
+//
+//  Created by Anirudh on 25/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ More Memorise A2/More Memorise A2/More_Memorise_A2App.swift
@@ -0,0 +1,17 @@
+//
+//  More_Memorise_A2App.swift
+//  More Memorise A2
+//
+//  Created by Anirudh on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct More_Memorise_A2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
